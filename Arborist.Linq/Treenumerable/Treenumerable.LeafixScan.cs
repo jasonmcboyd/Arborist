8b7af1032bcde489af96859a03a42d1f17dcdf7a
Jason Boyd <jason.boyd.ce@gmail.com>
2024-01-23 20:01:47 -0800

    Stubbed out LeafixScan

---
+++ Arborist.Linq/Treenumerable/Treenumerable.LeafixScan.cs
@@ -0,0 +1,25 @@
+﻿using Arborist.Linq.Treenumerators;
+using System;
+
+namespace Arborist.Linq
+{
+  public static partial class Treenumerable
+  {
+    //public static ITreenumerable<TNode> LeafixScan<TInner, TNode>(
+    //  this ITreenumerable<TInner> source,
+    //  Func<NodeVisit<TNode>, NodeVisit<TInner>, TNode> accumulator)
+    //  => TreenumerableFactory.Create(
+    //    source,
+    //    breadthFirstTreenumerator => throw new NotImplementedException(),
+    //    depthFirstTreenumerator => throw new NotImplementedException());
+
+    //public static ITreenumerable<TNode> LeafixScan<TInner, TNode>(
+    //  this ITreenumerable<TInner> source,
+    //  Func<NodeVisit<TNode>, NodeVisit<TInner>, TNode> accumulator,
+    //  TNode seed)
+    //  => TreenumerableFactory.Create(
+    //    source,
+    //    breadthFirstTreenumerator => throw new NotImplementedException(),
+    //    depthFirstTreenumerator => throw new NotImplementedException());
+  }
+}
