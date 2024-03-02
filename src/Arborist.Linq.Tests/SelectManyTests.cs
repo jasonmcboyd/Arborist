f121f3f728ae76c35cdf6871f9a76d25dc3ff874
Jason Boyd <jason.boyd.ce@gmail.com>
2024-03-01 19:54:50 -0800

    Beginning to work on SelectMany

--- src/Arborist.Linq.Tests/SelectManyTests.cs
+++ src/Arborist.Linq.Tests/SelectManyTests.cs
@@ -1,12 +1,136 @@
+using Arborist.Core;
 using Arborist.Nodes;
+using Arborist.TestUtils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Arborist.Linq.Tests
 {
   [TestClass]
   public class SelectManyTests
   {
+    static SelectManyTests()
+    {
+      _TreenumerableTestDataFactory = new TreenumerableTestDataFactory(TestTrees); 
+    }
+
+    private static TreenumerableTestDataFactory _TreenumerableTestDataFactory;
+
+    public static IEnumerable<object[]> GetTestData()
+      => _TreenumerableTestDataFactory.GetTestData();
+
+    public static string GetTestDisplayName(MethodInfo methodInfo, object[] data)
+      => _TreenumerableTestDataFactory.GetTestDisplayName(methodInfo, data);
+
+    private static TreeTestDefinition[] TestTrees =>
+      new TreeTestDefinition[]
+      {
+        // Empty Tree
+        new TreeTestDefinition
+        {
+          TreeString = "",
+          TestScenarios = new List<TestScenario>
+          {
+            // No skipping
+            new TestScenario
+            {
+              SchedulingPredicate = visit => SchedulingStrategy.TraverseSubtree,
+              TreenumerableMap = treenumerable => treenumerable.PruneAfter(_ => true),
+              Description = "Traverse all, prune after all",
+              ExpectedBreadthFirstResults = new MoveNextResult<string>[]
+              {
+              },
+              ExpectedDepthFirstResults = new MoveNextResult<string>[]
+              {
+              }
+            },
+          }
+        },
+
+        // Three root nodes, no children.
+        new TreeTestDefinition
+        {
+          TreeString = "a,b,c",
+          TestScenarios = new List<TestScenario>
+          {
+            // No skipping
+            new TestScenario
+            {
+              SchedulingPredicate = visit => SchedulingStrategy.TraverseSubtree,
+              TreenumerableMap = treenumerable => treenumerable.PruneAfter(_ => true),
+              Description = "Traverse all, prune after all",
+              ExpectedBreadthFirstResults = new MoveNextResult<string>[]
+              {
+                (TreenumeratorState.SchedulingNode, "a", 0, (0, 0), (0, 0)),
+                (TreenumeratorState.SchedulingNode, "b", 0, (1, 0), (1, 0)),
+                (TreenumeratorState.SchedulingNode, "c", 0, (2, 0), (2, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 1, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 2, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "b", 1, (1, 0), (1, 0)),
+                (TreenumeratorState.VisitingNode,   "b", 2, (1, 0), (1, 0)),
+                (TreenumeratorState.VisitingNode,   "c", 1, (2, 0), (2, 0)),
+                (TreenumeratorState.VisitingNode,   "c", 2, (2, 0), (2, 0)),
+              },
+              ExpectedDepthFirstResults = new MoveNextResult<string>[]
+              {
+                (TreenumeratorState.SchedulingNode, "a", 0, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 1, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 2, (0, 0), (0, 0)),
+                (TreenumeratorState.SchedulingNode, "b", 0, (1, 0), (1, 0)),
+                (TreenumeratorState.VisitingNode,   "b", 1, (1, 0), (1, 0)),
+                (TreenumeratorState.VisitingNode,   "b", 2, (1, 0), (1, 0)),
+                (TreenumeratorState.SchedulingNode, "c", 0, (2, 0), (2, 0)),
+                (TreenumeratorState.VisitingNode,   "c", 1, (2, 0), (2, 0)),
+                (TreenumeratorState.VisitingNode,   "c", 2, (2, 0), (2, 0)),
+              }
+            },
+            // Skip subtree
+            new TestScenario
+            {
+              SchedulingPredicate = visit => SchedulingStrategy.SkipSubtree,
+              TreenumerableMap = treenumerable => treenumerable.PruneAfter(_ => true),
+              Description = "Skip all subtrees, prune after all",
+              ExpectedBreadthFirstResults = new MoveNextResult<string>[]
+              {
+              },
+              ExpectedDepthFirstResults = new MoveNextResult<string>[]
+              {
+                (TreenumeratorState.SchedulingNode, "a", 0, (0, 0), (0, 0)),
+                (TreenumeratorState.SchedulingNode, "b", 0, (1, 0), (0, 0)),
+                (TreenumeratorState.SchedulingNode, "c", 0, (2, 0), (0, 0)),
+              }
+            },
+          }
+        },
+
+        // Two-level complete ternary tree.
+        new TreeTestDefinition
+        {
+          TreeString = "a(b,c,d)",
+          TestScenarios = new List<TestScenario>
+          {
+            // No skipping
+            new TestScenario
+            {
+              SchedulingPredicate = visit => SchedulingStrategy.TraverseSubtree,
+              TreenumerableMap = treenumerable => treenumerable.PruneAfter(_ => true),
+              Description = "Traverse all, prune after all",
+              ExpectedBreadthFirstResults = new MoveNextResult<string>[]
+              {
+              },
+              ExpectedDepthFirstResults = new MoveNextResult<string>[]
+              {
+                (TreenumeratorState.SchedulingNode, "a", 0, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 1, (0, 0), (0, 0)),
+                (TreenumeratorState.VisitingNode,   "a", 2, (0, 0), (0, 0)),
+              }
+            },
+          }
+        }
+      };
+
     [TestMethod]
     public void SelectMany()
     {
--- src/Arborist.Linq/Treenumerators/SelectMany/SelectManyDepthFirstTreenumerator.cs
+++ src/Arborist.Linq/Treenumerators/SelectMany/SelectManyDepthFirstTreenumerator.cs
@@ -17,39 +17,83 @@ namespace Arborist.Linq.Treenumerators
 
     public readonly Func<NodeVisit<TInner>, TNode> _Selector;
 
-    private readonly Stack<NodeVisit<TNode>> _Stack = new Stack<NodeVisit<TNode>>();
+    private Stack<ITreenumerator<TInner>> _PriorNodeTreenumerators = new Stack<ITreenumerator<TInner>>();
+
+    private ITreenumerator<TInner> _NodeTreenumerator;
+
+    private NodePosition _NodeTreenumeratorStartingOriginalPosition = (0, 0);
+    private NodePosition _NodeTreenumeratorStartingPosition = (0, 0);
 
     protected override bool OnMoveNext(SchedulingStrategy schedulingStrategy)
     {
-      throw new NotImplementedException();
-      //while (InnerTreenumerator.MoveNext(schedulingStrategy))
-      //{
-      //  using (var currentEnumerator = InnerTreenumerator.Node.Node.GetDepthFirstTreenumerator())
-      //  {
-      //    NodeVisit<TNode>? previousVisit = null;
+      if (State == TreenumeratorState.EnumerationFinished)
+        return false;
 
-      //    while (currentEnumerator.MoveNext(SchedulingStrategy.ScheduleForTraversal))
-      //    {
-      //      if (previousVisit != null)
-      //      {
-      //        Current = previousVisit.Value;
-      //        previousVisit = currentEnumerator.Current.WithNode(_Selector(currentEnumerator.Current));
-      //        return true;
-      //      }
-      //    }
+      if (State == TreenumeratorState.EnumerationNotStarted)
+        return OnStartingEnumeration();
 
-      //    if (previousVisit != null)
-      //      _Stack.Push(previousVisit.Value);
-      //  }
-      //}
+      if (_NodeTreenumerator.MoveNext(schedulingStrategy))
+      {
+        UpdateState();
 
-      //while (_Stack.Count > 0)
-      //{
-      //  Current = _Stack.Pop();
-      //  return true;
-      //}
+        return true;
+      }
 
-      //return false;
+      return MoveNextInnerSubtree();
+    }
+
+    private bool OnStartingEnumeration()
+    {
+      return MoveNextInnerSubtree();
+    }
+
+    private bool MoveNextInnerSubtree()
+    {
+      while (true)
+      {
+        if (_NodeTreenumerator != null)
+        {
+          _NodeTreenumeratorStartingOriginalPosition = OriginalPosition;
+          _NodeTreenumeratorStartingPosition = Position;
+        }
+
+        if (InnerTreenumerator.MoveNext(SchedulingStrategy.TraverseSubtree))
+        {
+          if (InnerTreenumerator.VisitCount != 1)
+            continue;
+
+          _NodeTreenumerator = InnerTreenumerator.Node.GetDepthFirstTreenumerator();
+
+          while (true)
+          {
+            if (_NodeTreenumerator.MoveNext(SchedulingStrategy.TraverseSubtree))
+            {
+              UpdateState();
+
+              return true;
+            }
+
+            _NodeTreenumerator.Dispose();
+            _NodeTreenumerator = null;
+          }
+        }
+
+        return false;
+      }
+    }
+
+    private void UpdateState()
+    {
+      State = _NodeTreenumerator.State;
+
+      if (State != TreenumeratorState.EnumerationFinished)
+      {
+        Node = _Selector(_NodeTreenumerator.ToNodeVisit());
+        VisitCount = _NodeTreenumerator.VisitCount;
+        OriginalPosition = _NodeTreenumerator.OriginalPosition + _NodeTreenumeratorStartingOriginalPosition;
+        Position = _NodeTreenumerator.Position + _NodeTreenumeratorStartingPosition;
+        SchedulingStrategy = _NodeTreenumerator.SchedulingStrategy;
+      }
     }
   }
 }
