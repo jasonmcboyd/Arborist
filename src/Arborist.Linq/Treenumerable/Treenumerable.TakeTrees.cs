fe5930c7177e773240d6e24f3514c65db6e9b7c7
Jason Boyd <jason.boyd.ce@gmail.com>
2024-04-07 22:28:09 -0700

    Mostly fixed TakeTrees

--- src/Arborist.Linq/Treenumerable/Treenumerable.TakeTrees.cs
+++ src/Arborist.Linq/Treenumerable/Treenumerable.TakeTrees.cs
@@ -4,9 +4,14 @@ namespace Arborist.Linq
 {
   public static partial class Treenumerable
   {
+    // TODO:
+    // This will still enumerate all the tree roots despite skipping the trees.
+    // It is entirely possible for the list to be algorithmically generated and be infinite;
+    // That would prevent this from ever completing. I Need to think of a solution for
+    // this.
     public static ITreenumerable<T> TakeTrees<T>(
       this ITreenumerable<T> source,
       int count)
-      => source.PruneBefore(visit => visit.OriginalPosition.SiblingIndex >= count);
+      => source.PruneBefore(visit => visit.OriginalPosition.Depth == 0 && visit.OriginalPosition.SiblingIndex >= count);
   }
 }
