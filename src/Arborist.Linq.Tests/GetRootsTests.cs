e9e8f609bd702d1b6d0c1a71c9669ad4dd46305f
Jason Boyd <jason.boyd.ce@gmail.com>
2024-05-05 22:02:57 -0700

    Added tests for GetRoots

---
+++ src/Arborist.Linq.Tests/GetRootsTests.cs
@@ -0,0 +1,46 @@
+using Arborist.SimpleSerializer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Arborist.Linq.Tests
+{
+  [TestClass]
+  public class GetRootsTests
+  {
+    public static IEnumerable<object[]> GetTestData()
+    {
+      yield return new object[] { "a", new[] { "a" } };
+      yield return new object[] { "a,b,c", new[] { "a", "b", "c" } };
+      yield return new object[] { "a(b,c)", new[] { "a" } };
+      yield return new object[] { "a(b(c))", new[] { "a" } };
+      yield return new object[] { "a(b,c),d(e,f)", new[] { "a", "d" } };
+      yield return new object[] { "a,b(c),d(e(f))", new[] { "a", "b", "d" } };
+    }
+
+    public static string GetTestDisplayName(MethodInfo methodInfo, object[] data)
+    {
+      return data[0].ToString();
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetTestDisplayName))]
+    public void GetRoots(
+      string treeString,
+      string[] expected)
+    {
+      // Arrange
+      var treenumerable = TreeSerializer.Deserialize(treeString);
+
+      // Act
+      var actual =
+        treenumerable
+        .GetRoots()
+        .ToArray();
+
+      // Assert
+      CollectionAssert.AreEqual(expected, actual);
+    }
+  }
+}
