fbeda10fcc1869d08081ed0b455d75ff9464d3c7
Jason Boyd <jason.boyd.ce@gmail.com>
2024-07-18 19:25:31 -0700

    Added CountTrees

---
+++ src/Arborist.Linq/Treenumerable/Treenumerable.CountTrees.cs
@@ -0,0 +1,11 @@
+﻿using Arborist.Core;
+using System.Linq;
+
+namespace Arborist.Linq
+{
+  public static partial class Treenumerable
+  {
+    public static int CountTrees<TNode>(this ITreenumerable<TNode> source)
+      => source.GetRoots().Count();
+  }
+}
