a08cc1968b0c121ceef039b5dfe8d88f35a1ba6f
Jason Boyd <jason.boyd.ce@gmail.com>
2024-07-18 19:14:26 -0700

    Added CountNodes

---
+++ src/Arborist.Linq/Treenumerable/Treenumerable.CountNodes.cs
@@ -0,0 +1,23 @@
+﻿using Arborist.Core;
+using Arborist.Linq.Extensions;
+using System;
+using System.Linq;
+
+namespace Arborist.Linq
+{
+  public static partial class Treenumerable
+  {
+    public static int CountNodes<TNode>(this ITreenumerable<TNode> source)
+      => source.CountNodes(_ => true);
+
+    public static int CountNodes<TNode>(
+      this ITreenumerable<TNode> source,
+      Func<NodeAndPosition<TNode>, bool> predicate)
+    {
+      if (source == null)
+        return 0;
+
+      return source.Select(visit => visit.ToNodeAndPosition()).Where(nodeVisit => predicate(nodeVisit.Node)).PreOrderTraversal().Count();
+    }
+  }
+}
