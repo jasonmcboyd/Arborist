3799568b1d4876d9b07779e007ff59f65b29dc42
Jason Boyd <jason.boyd.ce@gmail.com>
2024-12-14 10:53:25 -0800

    Fixed comment

--- src/Arborist.Trees/NDecrementTree.cs
+++ src/Arborist.Trees/NDecrementTree.cs
@@ -4,8 +4,16 @@ using Arborist.Trees;
 namespace Arborist.Trees
 {
   /// <summary>
-  /// Interesting fact about this tree is that the width of the levels follows
-  /// Pascal's triangle.
+  /// Interesting fact about this tree is that the width of the levels are
+  /// related to Pascal's triangle.
+  /// 
+  /// For example:
+  /// 
+  /// If the depth is 3, the width of the levels are 1, 2, 1, which corresponds to the
+  /// third row of Pascal's triangle.
+  /// 
+  /// If the depth is 5, the width of the levels are 1, 4, 6, 4, 1, which corresponds to the
+  /// fifth row of Pascal's triangle.
   /// </summary>
   public class NDecrementTree : Treenumerable<int, int, NDecrementTreeNodeChildEnumerator>
   {
